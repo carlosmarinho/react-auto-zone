24e1882e7c08f92044b8333ee8a2f6c0b1993074
Carlos Marinho <carlos.marinho@energage.com>
2024-05-27 17:56:51 -0300

    improve hooks with useMemo

--- src/components/hooks/useVehicleMakes.ts
+++ src/components/hooks/useVehicleMakes.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchVehicleMakes } from "../../api";
 import { IVehicleMakeType } from "../Vehicle/types";
+import { useMemo } from "react";
 
 export const useVehicleMakes = (year: string) => {
   const { data: vehicleMakes, status } = useQuery<IVehicleMakeType[]>({
@@ -9,11 +10,14 @@ export const useVehicleMakes = (year: string) => {
     enabled: !!year,
   });
 
-  const makes =
-    vehicleMakes
-      ?.map((item) => ({ id: item.MakeId, name: item.MakeName }))
-      .sort((a, b) => a.name.localeCompare(b.name))
-      .filter((v, i, a) => a.findIndex((t) => t.id === v.id) === i) || [];
+  const makes = useMemo(
+    () =>
+      vehicleMakes
+        ?.map((item) => ({ id: item.MakeId, name: item.MakeName }))
+        .sort((a, b) => a.name.localeCompare(b.name))
+        .filter((v, i, a) => a.findIndex((t) => t.id === v.id) === i) || [],
+    [vehicleMakes]
+  );
 
   return { makes, status };
 };
--- src/components/hooks/useVehicleModels.ts
+++ src/components/hooks/useVehicleModels.ts
@@ -1,6 +1,7 @@
 import { IVehicleModelType } from "../Vehicle/types";
 import { fetchVehicleModels } from "../../api";
 import { useQuery } from "@tanstack/react-query";
+import { useMemo } from "react";
 
 const useVehicleModels = (make: string) => {
   const { data: vehicleModels, status } = useQuery<IVehicleModelType[]>({
@@ -9,11 +10,14 @@ const useVehicleModels = (make: string) => {
     enabled: !!make,
   });
 
-  const models =
-    vehicleModels
-      ?.map((item) => ({ id: item.Model_ID, name: item.Model_Name }))
-      .sort((a, b) => a.name.localeCompare(b.name))
-      .filter((v, i, a) => a.findIndex((t) => t.id === v.id) === i) || [];
+  const models = useMemo(
+    () =>
+      vehicleModels
+        ?.map((item) => ({ id: item.Model_ID, name: item.Model_Name }))
+        .sort((a, b) => a.name.localeCompare(b.name))
+        .filter((v, i, a) => a.findIndex((t) => t.id === v.id) === i) || [],
+    [vehicleModels]
+  );
 
   return { models, status };
 };
